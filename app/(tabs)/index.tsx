5984d0b46569f1976464d2dc065d23b6b5d54652
yuki-okamoto-13 <okamoto.yuki@eversense.co.jp>
2025-04-09 15:39:18 +0900

    タブデザイン設定

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,9 +5,6 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
     </View>
   );
 }
